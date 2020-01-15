d0bd33668ee14a16dc68c4c6a8cc1e138c1db16d
asdf <asdf>
2020-01-15 21:14:02 +0530

    trying env variables

--- projectx/settings.py
+++ projectx/settings.py
@@ -290,7 +290,7 @@ USE_TZ = True
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # DEFAULT_FROM_EMAIL = 'prajwalbarapatre13@gmail.com' # this is the sendgrid email
 
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
